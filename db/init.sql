61a615592ba26cd8f54ca42259a7b117fd2464db
Devin <405466675@qq.com>
2025-07-05 23:08:08 +0800

    fix: 数据库初始化逻辑添加已有则不创建逻辑

--- db/init.sql
+++ db/init.sql
@@ -1,14 +1,28 @@
--- 创建用户 (如果不存在)
-CREATE USER user0 WITH PASSWORD '123456';
+-- 创建用户 (条件创建)
+DO $$
+BEGIN
+  IF NOT EXISTS (SELECT FROM pg_catalog.pg_user WHERE usename = 'user0') THEN
+    CREATE USER user0 WITH PASSWORD '123456';
+  END IF;
+END
+$$;
 
--- 创建数据库并设置所有者
-CREATE DATABASE realworld WITH OWNER = user0 ENCODING = 'UTF8';
+-- 创建数据库 (条件创建)
+DO $$
+BEGIN
+  IF NOT EXISTS (SELECT FROM pg_database WHERE datname = 'realworld') THEN
+    CREATE DATABASE realworld WITH OWNER = user0 ENCODING 'UTF8';
+  ELSE
+    ALTER DATABASE realworld OWNER TO user0;
+  END IF;
+END
+$$;
 
--- 授予用户所有权限
+-- 授予权限
 GRANT ALL PRIVILEGES ON DATABASE realworld TO user0;
 
--- 授予用户对public schema的所有权限
+-- 设置数据库权限
 \c realworld
-GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO user0;
-GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO user0;
-GRANT ALL PRIVILEGES ON ALL FUNCTIONS IN SCHEMA public TO user0;
+REVOKE ALL ON SCHEMA public FROM PUBLIC;
+GRANT ALL ON SCHEMA public TO user0;
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON TABLES TO user0;
